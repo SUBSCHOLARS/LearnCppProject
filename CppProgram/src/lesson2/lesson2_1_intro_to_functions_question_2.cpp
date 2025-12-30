30a36dd77afccca23cd77df272ae0c43507508fc
Kota Hatanaka <kenkotan7722@gmail.com>
2025-12-30 13:53:19 +0900

    finishing lesson 2-1 question

---
+++ CppProgram/src/lesson2/lesson2_1_intro_to_functions_question_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void doB()
+{
+    std::cout<<"In doB()\n";
+}
+
+void doA()
+{
+    std::cout<<"In doA()\n";
+    doB();
+}
+
+int main()
+{
+    std::cout<<"Starting main()\n";
+    doA();
+    doB();
+    std::cout<<"Ending main()\n";
+
+    return 0;
+}
