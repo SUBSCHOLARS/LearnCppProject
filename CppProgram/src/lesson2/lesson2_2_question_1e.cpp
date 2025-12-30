8707adf6e3ab1b9af65febc46bc8714b94ca5926
Kota Hatanaka <kenkotan7722@gmail.com>
2025-12-30 20:13:40 +0900

    finishing lesson 2-2 questions

---
+++ CppProgram/src/lesson2/lesson2_2_question_1e.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+// this will create a keyword error
+// int return 5()
+// {
+//     return 5;
+// }
+
+int main()
+{
+    // std::cout << return 5() << '\n';
+
+    return 0;
+}
