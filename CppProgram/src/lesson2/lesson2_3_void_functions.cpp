19d255cbf94bbdb7daa5a441b65966f9bf6609d6
Kota Hatanaka <kenkotan7722@gmail.com>
2025-12-31 16:54:13 +0900

    finishing lesson 2-3

---
+++ CppProgram/src/lesson2/lesson2_3_void_functions.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+void printHi()
+{
+    std::cout<<"Hi"<<'\n';
+}
+
+int main()
+{
+    printHi();
+    return 0;
+}
---
+++ CppProgram/src/lesson2/lesson2_3_void_functions_error.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+void printHi()
+{
+    std::cout<<"Hi"<<'\n';
+}
+
+int main()
+{
+    // this is fine
+    printHi();
+    // compile error because printHi does not have return type
+    // std::cout<<printHi();
+}
