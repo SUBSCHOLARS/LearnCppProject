5c083c79703b7cc3a49c803777c7ed84edb5076b
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-01 16:55:12 +0900

    finishing lesson 2-7

---
+++ CppProgram/src/lesson2/lesson2_7_forward_declarations_and_definitions.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+// this cannot be compiled at all!
+int main()
+{
+    // std::cout<<"The sum of 3 and 4 is: "<< add(3,4)<<'\n';
+    return 0;
+}
+int add(int x, int y)
+{
+    return x+y;
+}
+
+// this can easily be solved by replacing add function beforehand the main function
+// but notice that this is no always possible
+// for example, in the large program, it is hard to figure out where to replace the function
+// furthermore if function A calls function B, and function B calls function A, it is logically impossible to satisfy the compiler just by replacing the location of functions
---
+++ CppProgram/src/lesson2/lesson2_7_forward_declarations_and_definitions_example.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+// function declaration includes return type, name, parameters, and semicolon, No function body!
+int add(int x, int y);
+// it can also looks like this
+int add_another(int, int);
+
+int main()
+{
+    std::cout<<"The sum of 3 and 4 is: "<<add(3,4)<<'\n'; // this works because we forward declared add() above
+    return 0;
+}
+int add(int x, int y)
+{
+    return x+y;
+}
