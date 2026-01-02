dca9b37d0c77daf35d17b9dadffed1438d4d096d
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-02 12:46:51 +0900

    finishing lesson 2-11, learning about header

---
+++ CppProgram/src/lesson2/lesson2_11/header_files/add.cpp
@@ -0,0 +1,6 @@
+﻿#include "add.h"
+
+int add(int x, int y)
+{
+    return x+y;
+}
---
+++ CppProgram/src/lesson2/lesson2_11/header_files/add.h
@@ -0,0 +1,4 @@
+﻿// We really should have a header guard here
+
+// This is the content of the .h file
+int add(int x, int y);
