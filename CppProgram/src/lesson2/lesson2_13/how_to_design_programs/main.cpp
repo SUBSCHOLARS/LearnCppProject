b6d71d8512a56ad5dbf0912286b27ea9d744b6e2
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-02 14:41:28 +0900

    learn how to design a program, the most important thing

---
+++ CppProgram/src/lesson2/lesson2_13/how_to_design_programs/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+// Full implementation of the getUserInput function
+int getUserInput()
+{
+    std::cout<<"Enter an integer: ";
+    int input{};
+    std::cin>>input;
+
+    return input;
+}
+char getMathematicalOperation()
+{
+    std::cout<<"Enter an operation: ";
+    char operation{};
+    std::cin>>operation;
+    return operation;
+}
+int calculateResult(int value, char operation, int second_value)
+{
+    int calculatedResult{};
+}
+
+int main()
+{
+    // Get first number from user
+    int value{getUserInput()};
+    std::cout<<value<<'\n';
+
+    // Get mathematical operation from user
+    char operation{getMathematicalOperation()};
+    std::cout<<operation<<'\n';
+
+    // Get second number from user
+    int second_value {getUserInput()};
+    std::cout<<second_value<<'\n';
+
+    // Calculate result
+    // calculateResult();
+
+    // Print result
+    // printResult();
+
+    return 0;
+}
