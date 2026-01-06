2bf07255cd95059d82681073808fd3f5c8cc2bcb
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-06 21:29:32 +0900

    finishing lesson 3-3 and create a file for lesson 3-1

---
+++ CppProgram/src/lesson3/lesson3_3/debugging_strategy/debugging_strategy.cpp
@@ -0,0 +1,10 @@
+﻿int main(){
+    // getNames(); // ask user to enter a bunch of names
+    // sortNames(); // sort them in alphabetical order
+    // printNames(); // print the sortedlist of names
+
+    // if names are printed in not A-Z order but Z-A order, then 
+    // the problem would be found in sortNames() functions
+
+    return 0;
+}
