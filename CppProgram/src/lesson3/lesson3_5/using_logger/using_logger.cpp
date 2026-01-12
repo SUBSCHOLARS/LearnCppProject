7eee98e4dbf9cd624cb0f9adb57b92bbd14e7bb8
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-12 23:32:52 +0900

    Add plog library as source files and learn about logger debugging

---
+++ CppProgram/src/lesson3/lesson3_5/using_logger/using_logger.cpp
@@ -0,0 +1,22 @@
+﻿#include <plog/Log.h> // Step1: include the logger headers
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <iostream>
+
+int getUserInput()
+{
+    PLOGD << "getUSerInput() called"; // PLOGD is defined by the plog library
+    std::cout<<"Enter a number: ";
+    int x {};
+    std::cin>>x;
+    return x;
+}
+int main()
+{
+    plog::init(plog::debug, "Logfile.txt"); // Step2: initalize the logger
+    PLOGD<<"main() called"; // Step3: Output to the log as if you were writing to the console
+
+    int x {getUserInput()};
+    std::cout<<"You entered: "<<x<<'\n';
+
+    return 0;
+}
