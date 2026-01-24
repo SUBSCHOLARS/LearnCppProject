e537fd647f4089c68e03fa037e6de0417785df52
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-24 23:07:44 +0900

    learning about call stack

---
+++ CppProgram/src/lesson3/lesson3_9/call_stack/call_stack.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void a()
+{
+    std::cout<<"a() called\n";
+}
+
+void b()
+{
+    std::cout<<"b() called\n";
+    a();
+}
+
+int main()
+{
+    a();
+    b();
+
+    return 0;
+}
