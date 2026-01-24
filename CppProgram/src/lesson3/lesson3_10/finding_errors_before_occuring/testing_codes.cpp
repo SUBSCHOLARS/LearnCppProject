517603366b230219aa9430aa1040392b24e806a3
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-24 23:35:16 +0900

    finishing lesson3

---
+++ CppProgram/src/lesson3/lesson3_10/finding_errors_before_occuring/testing_codes.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int add(int x, int y)
+{
+    return x+y;
+}
+
+void testadd()
+{
+    std::cout<<"This function shoud print: 2 0 0 -2\n";
+    std::cout<<add(1,1)<<' ';
+    std::cout<<add(-1,1)<<' ';
+    std::cout<<add(1,-1)<<' ';
+    std::cout<<add(-1,-1)<<' ';
+}
+
+int main()
+{
+    testadd();
+
+    return 0;
+}
---
+++ CppProgram/src/lesson3/lesson3_summary/question3/question3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+void d()
+{ // here
+}
+
+void c()
+{
+}
+
+void b()
+{
+	c();
+	d();
+}
+
+void a()
+{
+	b();
+}
+
+int main()
+{
+	a();
+
+	return 0;
+}
