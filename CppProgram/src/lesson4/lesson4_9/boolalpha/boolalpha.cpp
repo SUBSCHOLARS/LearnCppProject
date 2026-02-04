3eb8fdedbaeff949a3ed4085068c754d12a6b192
Kota Hatanaka <kenkotan7722@gmail.com>
2026-02-04 23:29:34 +0900

    learning about boolean values

---
+++ CppProgram/src/lesson4/lesson4_9/boolalpha/boolalpha.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout<<true<<'\n';
+    std::cout<<false<<'\n';
+
+    std::cout<<std::boolalpha; // print bools as true or false
+
+    std::cout<<true<<'\n';
+    std::cout<<false<<'\n';
+
+    // we can use std::noboolalpha to turn it back off :)
+
+    return 0;
+}
---
+++ CppProgram/src/lesson4/lesson4_9/inputting_boolean_values/inputting_boolean_values.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    bool b{}; // default initialize to false
+    std::cout<<"Enter a boolean value: ";
+    std::cin>>std::boolalpha;
+    std::cin>>b;
+    std::cout<<std::boolalpha;
+    std::cout<<"You entered: "<<b<<'\n';
+
+    return 0;
+}
