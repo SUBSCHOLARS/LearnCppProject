f7fc1412c92ab99c455db40e6ee5c8996c192072
Kota Hatanaka <kenkotan7722@gmail.com>
2026-02-05 14:35:34 +0900

    learning about implicit conversion and static cast

---
+++ CppProgram/src/lesson4/lesson4_12/int8_t_and_uint8_t/int8_t_and_uint8_t.cpp
@@ -0,0 +1,11 @@
+﻿#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    std::int8_t myInt {65};
+    std::cout<<myInt<<'\n'; // this will output A most likely because it is interpreted as char
+    std::cout<<static_cast<int>(myInt)<<'\n';
+
+    return 0;
+}
