ab05cb0c5a15b3b8cc1e4c4e857b86a072c26912
Kota Hatanaka <kenkotan7722@gmail.com>
2026-02-07 22:50:28 +0900

    learning about literals, and magic numbers

---
+++ CppProgram/src/lesson5/lesson5_2/floating_point_literals/floating_point_literals.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout<<5.0<<'\n'; // 5.0(no suffix) is type double(by default)
+    std::cout<<5.0f<<'\n'; // 5.0f(f suffix) is type float
+
+    float f {4.1}; // creates warning, maybe
+    return 0;
+}
---
+++ CppProgram/src/lesson5/lesson5_2/scientific_notation_for_floating_point_literals/two_ways_to_write_floating_point_literals.cpp
@@ -0,0 +1,9 @@
+﻿// write in decimal format
+double pi {3.14159}; // 3.14159 is a double literal in standard notation
+double d {-1.23}; // the literal can be negative
+double why {0.}; // syntactically acceptable, but avoid this because it is hard to see the decimal point
+// use 0.0 instead
+
+// write in e format
+double avogadro {6.02e23}; // 6.02 x 10^23 is a double literal in scientific notation
+double protonCharge{1.6e-19}; // charge on a proton is 1.6 x 10^-19
