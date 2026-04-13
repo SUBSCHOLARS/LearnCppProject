51a3d9b8ae1c96fdecdac35dd0a946833bb4a7b0
Kota Hatanaka <kenkotan7722@gmail.com>
2026-04-13 15:41:30 +0900

    learning about operator and operands, and its order

---
+++ CppProgram/src/lesson6/lesson6_1/order_of_evaluation_of_operands/order_of_evaluation_of_operands.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int getValue()
+{
+    std::cout << "Enter an integer: ";
+
+    int x {};
+    std::cin >> x;
+    return x;
+}
+
+void printCalculation(int x, int y, int z)
+{
+    std::cout << x + (y * z);
+}
+
+int main()
+{
+    printCalculation(getValue(), getValue(), getValue());
+    return 0;
+}
