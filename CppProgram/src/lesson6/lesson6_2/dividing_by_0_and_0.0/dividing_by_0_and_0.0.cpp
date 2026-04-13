0ccc06513e25b269e64f091467a017f17b9b2a64
Kota Hatanaka <kenkotan7722@gmail.com>
2026-04-13 16:05:47 +0900

    learning about division behavior

---
+++ CppProgram/src/lesson6/lesson6_2/dividing_by_0_and_0.0/dividing_by_0_and_0.0.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    constexpr int apples {12};
+    std::cout<<"You have "<<apples<<" apples. Enter how many people to divide them between: ";
+    int x {};
+    std::cin>>x;
+
+    std::cout<<"Each person gets "<<apples / x <<" whole apples.\n";
+
+    return 0;
+}
